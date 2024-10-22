38750d669118503a3c9b55818f8e43c3221f3188
siddhant <siddhant.sharma.21cse@bmu.edu.in>
2024-10-22 18:24:54 +0530

    Second commit

---
+++ client/src/services/index1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index1() {
+  return (
+    <div>index1</div>
+  )
+}
+
+export default index1
---
+++ client/src/utils/test4.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function test4() {
+  return (
+    <div>test4</div>
+  )
+}
+
+export default test4
