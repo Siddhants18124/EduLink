01868049416eb276ec412560a8e6ccb7ab0b97c4
ketanthakurr <ketanthakur603@gmail.com>
2024-11-29 23:41:47 +0530

    Question page updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+// Module------------------------------------
 import React from "react";
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer} from 'react-toastify';
+
+// Pages-------------------------------------
 import Signup from './pages/sign-up/Signup';
 import Login from './pages/login/Login';
 import Home from "./pages/home/Home";
 import Question from "./pages/question/Question";
 import Profile from "./pages/profile/Profile";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Resources from "./pages/resources/Resources";
 // import Resources from "./components/SearchBars/Resources";
 
 
@@ -20,6 +24,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/question" element={<Question />} />
+        <Route path="/resources" element={<Resources />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
